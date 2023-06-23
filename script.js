fdc0cfd78c0cff7fd3ec345d153b3e05386e6244
billydenura <billydenura03@gmail.com>
2023-06-23 20:32:56 +0700

    menambahkan env

--- script.js
+++ script.js
@@ -8,10 +8,15 @@ const getWeatherForecast = async (cityName) => {
         },
         });
         const data = await response.json();
+        let cityDescription = document.getElementById("city-description")
         console.log(data);
+        cityDescription.innerHTML = `Showing the weather of ${data.location.name}, ${data.location.region}, ${data.location.country}`
+        showCurrentWethear(data)
         showForeCast(data)
         return data;
     } catch (error) {
+        let cityDescription = document.getElementById("city-description")
+        cityDescription.innerHTML = "LOCATION NOT FOUND"
         console.log(error);
         return;
     }
@@ -27,23 +32,63 @@ getWeather.addEventListener("click",()=>{
 async function showForeCast(data){
     let days = data.forecast.forecastday
     for(i=0;i<days.length;i++){
-        let day = days[i].date
-        let weather = days[i].day.condition.text
-        let icon = days[i].day.condition.icon
-        let temp = days[i].day.avgtemp_c
-        let tempMax = days[i].day.maxtemp_c
-        let tempMin = days[i].day.mintemp_c
-        let humidity = days[i].day.avghumidity
+        let forecast = document.createElement("div")
+        let weatherForecast = document.getElementById("weather-forecast")
+        weatherForecast.append(forecast)
+        forecast.className = "weather-container"
+
+        let day = document.createElement("h3")
+        let weather = document.createElement("p")
+        let icon = document.createElement("img")
+        icon.className = "weather-image"
+        let temp = document.createElement("p")
+        let tempMaxMin = document.createElement("p")
+        let humidity = document.createElement("p")
 
 
+        day.innerHTML =`Weather of ${days[i].date}` 
+        weather.innerHTML =`"${days[i].day.condition.text}"` 
+        icon.src = days[i].day.condition.icon
+        temp.innerHTML =`Average Temperature: ${days[i].day.avgtemp_c} C` 
+        tempMaxMin.innerHTML =`(Maximum: ${days[i].day.maxtemp_c} C, Minimum ${days[i].day.mintemp_c} C)` 
+        humidity.innerHTML =`Avarage Humidity: ${days[i].day.avghumidity}%` 
+
+        forecast.append(day)
+        forecast.append(weather)
+        forecast.append(icon)
+        forecast.append(temp)
+        forecast.append(tempMaxMin)
+        forecast.append(humidity)
     }
 }
 
 async function showCurrentWethear(data){
-    let weather = data.condition.text
-    let icon = data.condition.icon
-    let temp = data.temp_c
-    let humidity = data.humidity
-    let lastUpdate = data.last_update
+    let forecast = document.createElement("div")
+    let weatherForecast = document.getElementById("current-weather")
+    weatherForecast.append(forecast)
+    forecast.className = "weather-container"
+
+    let day = document.createElement("h3")
+    let weather = document.createElement("p")
+    let icon = document.createElement("img")
+    icon.className = "weather-image"
+    let temp = document.createElement("p")
+    let humidity = document.createElement("p")
+    let lastUpdate = document.createElement("p")
+
+
+    day.innerHTML = "Current Weather"
+    weather.innerHTML =`"${ data.current.condition.text}"` 
+    icon.src = data.current.condition.icon
+    temp.innerHTML =`Average Temperature: ${data.current.temp_c} C` 
+    humidity.innerHTML =`Avarage Humidity: ${data.current.humidity}%` 
+    lastUpdate.innerHTML = `(update at ${ data.current.last_updated})`
+
+    forecast.append(day)
+    forecast.append(weather)
+    forecast.append(icon)
+    forecast.append(temp)
+    forecast.append(humidity)
+    forecast.append(lastUpdate)
 
 }
